acf159c64cb35ce094c2847a90ec463fb901bcd5
whdgu <whdgu@192.168.219.107>
2021-12-13 19:19:44 +0900

    버전 : 3.0 변경 : 버전 업 및 로그인 기능 개선 작성자 : 누구누구

--- Test826Java/src/TestJava.java
+++ Test826Java/src/TestJava.java
@@ -4,6 +4,6 @@ public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("Hello GIT world");
 		System.out.println("Version : 2.0");
-		System.out.println("�α��� �� !!!");
+		System.out.println("인코딩 변경 및 로그인 기능 업그레이드 !!!");
 	}
 }
