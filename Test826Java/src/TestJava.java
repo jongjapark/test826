44a9f1774c1dfc4301f74406eeb1bf4135d9aa45
whdgu <whdgu@192.168.219.107>
2021-12-13 19:27:15 +0900

    버전 6.0 변경: 보기기능 추가

--- Test826Java/src/TestJava.java
+++ Test826Java/src/TestJava.java
@@ -3,7 +3,8 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GIT world");
-		System.out.println("Version : 5.0");
+		System.out.println("Version : 6.0");
 		System.out.println("인코딩 변경 및 로그인 기능 업그레이드 !!!");
+		System.out.println("보기 기능 추가!");
 	}
 }
